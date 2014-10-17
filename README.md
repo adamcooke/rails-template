e65b033f752388618d1cfd5dec8eb086c615a9db
Adam Cooke <adam@atechmedia.com>
2014-10-17 20:49:34 +0100

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rails Application Template
+
+This is my template for generating new applications. I try to keep it up to date with the latest things which I often use in an application or which are always useful.
+
+## Features
+
+* Sets up an initial git repository
+* Removes some gems which I don't take advantage of from the default Gemfile (including spring, sdoc, turbolinks and jbuilder)
+* Adds a number of gems which I always use
+* Uses HAML rather than ERB (including replacing the default application layout)
+* Uses SCSS rather than CSS
+* Uses Coffeescript rather than plain JS
+* Disables all generators
+* Autoloads from /lib
+* Adds environment specific configuration for development to config/environment.yml
---
+++ template.rb
@@ -0,0 +1,113 @@
+#
+# Add the clean application
+#
+git :init
+git :add => "."
+git :commit => %Q{-m 'Initial commit'}
+
+#
+# Remove gems from the Gemfile which we don't want anything to do with
+#
+original_gemfile = File.read("Gemfile")
+new_gemfile = original_gemfile.split("\n").select { |line| line =~ /\A\s*[a-z]/ }
+gems_to_remove = ['spring', 'turbolinks', 'jbuilder', 'sdoc']
+new_gemfile = new_gemfile.reject do |line|
+  line =~ /gem ['"](#{gems_to_remove.join('|')})['"]/
+end
+File.open("Gemfile", 'w') { |f| f.write(new_gemfile.join("\n"))}
+
+#
+# Add gems
+#
+gem 'haml', '~> 4.0.5'
+gem 'nifty-utils', '~> 1.0.2'
+gem 'nifty-attachments', '~> 1.0.3'
+gem 'nilify_blanks', '~> 1.1.0'
+gem 'authie', '~> 1.0.0'
+gem 'annotate', '~> 2.6.5'
+gem 'kaminari', '~> 0.16.1'
+gem 'chronic', '~> 0.10.2'
+gem 'dynamic_form'
+gem 'activevalidators'
+gem 'bcrypt', '~> 3.1.7'
+gem 'rails_env_config', '~> 1', :group => :development
+
+#
+# Generate the nifty attachments migration
+#
+generate("nifty:attachments:migration")
+
+#
+# Replace the default template with a HAML one
+#
+inside "app/views/layouts" do
+  run "rm application.html.erb"
+  file "application.html.haml", <<-CODE
+!!!
+%html
+  %head
+    %title #{@page_title}
+    = javascript_include_tag 'application'
+    = stylesheet_link_tag 'application'
+    = csrf_meta_tags
+  %body
+    = flash_display
+    = yield  
+CODE
+end
+
+#
+# Copy the default CSS to SCSS
+#
+inside "app/assets/stylesheets" do
+  run "mv application.css application.scss"
+end
+
+#
+# Copy the default JS to coffeescript
+#
+inside "app/assets/javascripts" do
+  file_contents = File.read("application.js").gsub(/\/\//, '##')
+  File.open("application.coffee", "w") { |f| f.write(file_contents) }
+  run "rm application.js"
+end
+
+#
+# Remove turbolinks from application.coffee
+#
+inside "app/assets/javascripts" do
+  content = File.readlines("application.coffee").reject do |line|
+    line =~ /\A##= require turbolinks/
+  end
+  File.open("application.coffee", 'w') { |f| f.write(content.join) }
+end
+
+#
+# Disable all generators
+#
+environment <<-CODE
+config.generators do |g|
+      g.orm             :active_record
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+    end
+CODE
+
+#
+# Add autoload path
+#
+environment 'config.autoload_paths += %W(#{config.root}/lib)'
+
+#
+# Add a default environment.yml for configuration
+#
+run "echo 'config/environment.yml' >> .gitignore"
+file "config/environment.yml", "EXAMPLE_VAR: Hello there!\n"
+
+#
+# Commit before we begin
+#
+git :add => "."
+git :commit => %Q{-m 'Setup application'}
